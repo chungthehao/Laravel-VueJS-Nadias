e0df767bbe2b6d9b850d42be7cebc42d72ba4ede
--local <chungthehao@gmail.com>
2019-09-02 23:09:33 +0700

    Building a CRUD component

--- resources/js/components/CategoryManager.vue
+++ resources/js/components/CategoryManager.vue
@@ -1,18 +1,45 @@
 <template>
-    <div>
-        <div v-for="category in categories"
+    <form>
+        <div v-for="(category, index) in categories"
             :key="category.id">
-            {{ category.name }}
+            <input type="text" v-model="category.name">
+            <input type="number" v-model="category.display_order">
+            <a @click="removeCategory(index)" class="remove">delete</a>
+
+            <div>
+                <img :src="`/images/${category.image}`" width="100">
+                <input type="text" v-model="category.image">
+            </div>
+
+            <hr>
         </div>
-    </div>
+    </form>
 </template>
 
 <script>
 export default {
-    props: ['categories']
+    props: ['initialCategories'],
+    data() {
+        return {
+            categories: _.cloneDeep(this.initialCategories)
+        };
+    },
+    methods: {
+        removeCategory(index) {
+            if (confirm('Are you sure?')) {
+                this.categories.splice(index, 1);
+            }
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+    img {
+        vertical-align: middle;
+    }
 
+    hr {
+        margin-bottom: 30px;
+    }
 </style>
