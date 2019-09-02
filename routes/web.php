dc73e41678646e88abea8a6427ab4524cb14a95e
--local <chungthehao@gmail.com>
2019-09-02 07:53:05 +0000

    Integrating a layout

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Auth::routes(['verify' => true]); // Tạo ra 3 routes có tên: verification.resend/notice/verify
