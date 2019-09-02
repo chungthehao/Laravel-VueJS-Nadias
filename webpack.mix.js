31c3468c77e4824e2dcd636857a4fd85a74e5982
--local <chungthehao@gmail.com>
2019-09-02 03:34:51 +0000

    Cache busting and Browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,14 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+// https://laravel.com/docs/5.8/mix#versioning-and-cache-busting
+// - Tu kem theo unique query string de buoc browser ng dung tren production
+// lay dung version moi cua file css js (link tao bang mix helper)
+if (mix.inProduction()) {
+    mix.version();
+}
+
+// https://laravel.com/docs/5.8/mix#browsersync-reloading
+// Tu refresh khi css, js (duoc compile bang mix) hay php files thay doi
+mix.browserSync('dev.nadias');
