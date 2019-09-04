b99fcc3b0ed2d2fa2e168de8cd83ea345c05f652
--local <chungthehao@gmail.com>
2019-09-04 19:26:25 +0700

    Working with related models

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -65,14 +65,16 @@ class CategoryController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Display the items in a category.
      *
      * @param  \App\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function show(Category $category)
+    public function items(Category $category)
     {
-        //
+        return $category->menuItems->map(function ($item) {
+            return $item->only(['id', 'name']);
+        });
     }
 
     /**
